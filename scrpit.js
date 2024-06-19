5d8a5c40d4549604d25dfe9ccf2061a836008e5b
Your-Name-p <narutoko26.6@gmail.com>
2024-06-19 09:09:37 +0530

    Add files via upload
    check it 📤

---
+++ scrpit.js
@@ -0,0 +1,50 @@
+// let sum = function(num1, num2){
+//     return num1 + num2;
+// }
+// let sumthree = function(num1, num2, num3 ,sumofTwo){
+//     let sum1 = sumofTwo(num1, num2);
+//     return sumofTwo(sum1, num3)
+// }
+// console.log(sumthree(1+1+1 ,sum));
+// let sum1 =(num1,num2)=>{
+// return num1+num2
+// }
+
+
+
+// console.log(sum1(2,5))
+// let timeout=setTimeout(() => {
+//     console.log(`itts workiing ${timeout}`);
+// }, 5000);
+// clearTimeout(timeout)
+
+
+
+//  let id=setInterval(() => {
+//     console.log("goooo");
+// }, 1000);
+// // clearInterval(id)
+// setTimeout(() => {
+//     clearInterval(id);
+// }, 5000);
+
+// let button=document.querySelector('#Btn');
+// // let test = event=>console.log("yes it work ");
+// button.addEventListener("click",event=>
+// console.log("clicked me")
+// );
+// button.removeEventListener('click',test);
+// let arr = [1,9,97,5,4,6]
+// let even = arr.filter((value,index)=>{
+//     return value %2===0;
+// })
+// console.log(even);
+//  let a = [1,2,3,4,5,6,7]
+// function mulitply(num1,num2){
+// return num1*num2;
+// };
+// console.log(mulitply(2,3));
+let button= document.querySelector('#Btn');
+button=addEventListener('click',event=>{
+set
+})
